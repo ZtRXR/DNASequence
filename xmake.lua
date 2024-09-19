73e835199afb6ee4a8c9f1c22db472c15e3d1bb5
Zengtudor <zengtudor@outlook.com>
2024-09-19 10:05:29 +0800

    update

--- xmake.lua
+++ xmake.lua
@@ -7,6 +7,7 @@ set_rundir("./")
 
 if is_plat("windows")then 
     -- 注意，这里是启用AVX512指令集矢量化加速，只有新2010年后的CPU支持，但是可以大大加快批量指令加速，可以试试将下面的--删除
+    -- 但是似乎这个程序目前任然是IO密集型，性能差不多
     -- add_cxxflags("/arch:AVX512") 
 end
 
