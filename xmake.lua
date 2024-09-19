5983f5488450cc3f2cc792862029d9165c13e26d
Zengtudor <zengtudor@outlook.com>
2024-09-19 10:43:18 +0800

    update

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,5 @@
 add_rules("mode.debug","mode.release")
-
-add_includedirs("src/tools")
 set_languages("c++23")
-
-set_rundir("./")
 
 if is_plat("windows")then 
     -- 注意，这里是启用AVX512指令集矢量化加速，只有新2010年后的CPU支持，但是可以大大加快批量指令加速，可以试试将下面的--删除
@@ -13,6 +9,9 @@ elseif is_plat("linux") or is_plat("mingw") or is_plat("clang")then
     -- 启用AVX512指令集矢量化加速，可能会导致无法在除了编译本程序的电脑上运行，出现兼容问题
     -- add_cxxflags("-march=native")
 end 
+
+add_includedirs("src/tools")
+set_rundir("./")
 
 target("dna")
     add_files("src/main.cpp")
